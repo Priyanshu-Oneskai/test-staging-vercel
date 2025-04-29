e448927ad3cf30db00e5398cf4d2f20726b6d04a
Priyanshu Kundu <priyanshu.kundu@oneskai.com>
2025-04-29 11:30:52 +0530

    pushing in the staging env

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Vite + React</h1>
-      <h1>Vite + React</h1>
+      <h2>Hi there I am priyanshu</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
